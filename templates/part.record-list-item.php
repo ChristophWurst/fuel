1d08531965b3ae307d2af6dbd37b6de4a0ea340e
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-08-05 20:46:00 +0200

    improved record list layout

--- templates/part.record-list-item.php
+++ templates/part.record-list-item.php
@@ -13,6 +13,22 @@
 <script id="record-list-item-template" type="text/html">
 	<a href="#vehicle/<%= vehicleId %>/record/<%= id %>"
 	   class="<%= active ? 'active' : ''%>">
-		<%= id %>
+		<div class="record-container">
+			<div class="record-date">
+				<%= date %>
+			</div>
+			<div class="record-odo">
+				<%= odo %>km
+			</div>
+			<div class="record-fuel">
+				<%= fuel %>l
+			</div>
+			<div class="record-distance">
+				+<%= 140 %>km
+			</div>
+			<div class="record-consumption">
+				<%= 5.8 %>l/100km
+			</div>
+		</div>
 	</a>
 </script>
