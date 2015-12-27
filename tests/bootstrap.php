06ef97ef119e75684a01f2e0fcb7707738503349
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-12-27 14:34:41 +0100

    fix unit test execution

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ownCloud - fuel
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @copyright Christoph Wurst 2015
+ */
+
+require_once __DIR__ . '/../../../tests/bootstrap.php';
