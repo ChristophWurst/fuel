7fde2b9feb1f74fccc93f30b931fc1660af61256
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-01-30 14:15:02 +0100

    fix unit tests

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace OCA\Fuel\AppInfo;
-
 /**
  * ownCloud - fuel
  *
@@ -9,24 +7,26 @@ namespace OCA\Fuel\AppInfo;
  * later. See the COPYING file.
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
- * @copyright Christoph Wurst 2015
+ * @copyright Christoph Wurst 2015, 2016
  */
-\OCP\App::addNavigationEntry([
-	// the string under which your app will be referenced in owncloud
-	'id' => 'fuel',
 
-	// sorting weight for the navigation. The higher the number, the higher
-	// will it be listed in the navigation
-	'order' => 10,
+namespace OCA\AbceDf\AppInfo;
 
-	// the route that will be shown on startup
-	'href' => \OCP\Util::linkToRoute('fuel.page.index'),
+use OCP\AppFramework\App;
 
-	// the icon that will be shown in the navigation
-	// this file needs to exist in img/
-	'icon' => \OCP\Util::imagePath('fuel', 'app.svg'),
+$app = new App('Fuel');
+$container = $app->getContainer();
 
-	// the title of your application. This will be used in the
-	// navigation or on the settings page of your app
-	'name' => \OC_L10N::get('fuel')->t('Fuel')
-]);
+$container->query('OCP\INavigationManager')->add(function () use ($container) {
+	$urlGenerator = $container->query('OCP\IURLGenerator');
+	$l10n = $container->query('OCP\IL10N');
+	return [
+		'id' => 'fuel',
+		'order' => 10,
+		// the route that will be shown on startup
+		'href' => $urlGenerator->linkToRoute('fuel.page.index'),
+		// the icon that will be shown in the navigation
+		'icon' => $urlGenerator->imagePath('fuel', 'app.svg'),
+		'name' => $l10n->t('Fuel'),
+	];
+});
